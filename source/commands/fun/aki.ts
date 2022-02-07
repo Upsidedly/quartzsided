abe8ce2e54950c2254df326b58c784f621adc739
Upsided <matthewlloydw@gmail.com>
2022-02-07 22:11:46 +0000

    Fixed wrong name for akinator.

--- source/commands/fun/aki.ts
+++ source/commands/fun/aki.ts
@@ -3,8 +3,8 @@ import { CommandInteraction, MessageEmbed } from 'discord.js';
 import akinator from 'discord.js-akinator';
 
 quartzsided.newCommand('CHAT_INPUT', {
-    name: 'snipe',
-    description: 'Snipe the recently deleted message in the channel.',
+    name: 'aki',
+    description: 'Play a game of akinator!',
     developmental: true,
     run: (handler: Handler, inter: CommandInteraction) => {
         akinator(inter, { language: 'en', useButtons: 'true', embedColor: 'BLACK' })
